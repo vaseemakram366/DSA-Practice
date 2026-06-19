9faddff3d6271aaf792a39eec676cc4ad841a9d0
Vaseem Akram <vaseemakrama366@gmail.com>
2026-06-19 21:19:46 +0530

    Update DSA solutions

--- Week1/Day3/4.py
+++ Week1/Day3/4.py
@@ -4,23 +4,12 @@
 
 
 
-# year = int(input("Enter a year: "))
+year = int(input("Enter a year: "))
 
-# if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-#     print("Leap Year")
-# else:
-#     print("Not a Leap Year")
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
 
 
 
-def divisible_by_3_and_5(num):
-    return num % 3 == 0 and num % 5 == 0
-
-def atm_withdrawal(balance, amount):
-    return amount <= balance
-
-def admission_eligible(marks, sports_quota):
-    return marks >= 70 or sports_quota
-
-def is_leap_year(year):
-    return (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
