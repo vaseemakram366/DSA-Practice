954190f5c55e9036e9d3263484acecc4cf37bf8c
Vaseem Akram <vaseemakrama366@gmail.com>
2026-06-29 19:54:39 +0530

    Day 9: Solved 6 LeetCode array problems with optimal Python solutions

---
+++ Week3/1.py
@@ -0,0 +1,17 @@
+# Topic: Searching and sorting algorithms
+
+# 215. Kth Largest Element in an Array
+
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_heap = []
+
+        for num in nums:
+            heapq.heappush(min_heap, num)
+
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        return min_heap[0]
---
+++ Week3/2.py
@@ -0,0 +1,15 @@
+
+# 1464. Maximum Product of Two Elements in an Array
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        first = second = 0
+
+        for num in nums:
+            if num >= first:
+                second = first
+                first = num
+            elif num > second:
+                second = num
+
+        return (first - 1) * (second - 1)
